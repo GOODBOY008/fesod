GOODBOY008/fastexcel

feat(examples): refactor example module

fesod-examples/fesod-sheet-examples/src/main/java/org/apache/fesod/sheet/examples/read/listeners/ExceptionListener.java
@@ -1,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.sheet.examples.read.listeners;
+
+import com.alibaba.fastjson2.JSON;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.fesod.sheet.context.AnalysisContext;
+import org.apache.fesod.sheet.examples.read.data.ExceptionDemoData;
+import org.apache.fesod.sheet.exception.ExcelDataConvertException;
+import org.apache.fesod.sheet.metadata.data.ReadCellData;
+import org.apache.fesod.sheet.read.listener.ReadListener;
+import org.apache.fesod.sheet.util.ListUtils;
+
+/**
+ * Listener for handling read and convert exceptions.
+ */
+@Slf4j
+public class ExceptionListener implements ReadListener<ExceptionDemoData> {
+
+    private static final int BATCH_COUNT = 100;
+
+    private List<ExceptionDemoData> cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+
+    @Override
+    public void onException(Exception exception, AnalysisContext context) {
+        log.error("Parsing failed, but continue parsing the next line: {}", exception.getMessage());
+        if (exception instanceof ExcelDataConvertException) {
+            ExcelDataConvertException excelDataConvertException = (ExcelDataConvertException) exception;
+            log.error(
+                    "Parsing exception in row {}, column {}, data: {}",
+                    excelDataConvertException.getRowIndex(),
+                    excelDataConvertException.getColumnIndex(),
+                    excelDataConvertException.getCellData());
+        }
+    }
+
+    @Override
+    public void invokeHead(Map<Integer, ReadCellData<?>> headMap, AnalysisContext context) {
+        log.info("Parsed a header row: {}", JSON.toJSONString(headMap));
+    }
+
+    @Override
+    public void invoke(ExceptionDemoData data, AnalysisContext context) {
+        log.info("Parsed a data row: {}", JSON.toJSONString(data));
+        cachedDataList.add(data);
+        if (cachedDataList.size() >= BATCH_COUNT) {
+            saveData();
+            cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+        }
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        saveData();
+        log.info("All data parsing completed!");
+    }
+
+    private void saveData() {
+        log.info("{} records saved to database!", cachedDataList.size());
+    }
+}

fesod-examples/fesod-sheet-examples/src/main/java/org/apache/fesod/sheet/examples/web/UploadDataListener.java
@@ -1,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.sheet.examples.web;
+
+import com.alibaba.fastjson2.JSON;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.fesod.sheet.context.AnalysisContext;
+import org.apache.fesod.sheet.read.listener.ReadListener;
+import org.apache.fesod.sheet.util.ListUtils;
+
+/**
+ * Listener for web upload examples.
+ */
+@Slf4j
+public class UploadDataListener implements ReadListener<UploadData> {
+
+    private static final int BATCH_COUNT = 100;
+    private List<UploadData> cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+    private UploadDAO uploadDAO;
+
+    public UploadDataListener(UploadDAO uploadDAO) {
+        this.uploadDAO = uploadDAO;
+    }
+
+    @Override
+    public void invoke(UploadData data, AnalysisContext context) {
+        log.info("Parsed a data row: {}", JSON.toJSONString(data));
+        cachedDataList.add(data);
+        if (cachedDataList.size() >= BATCH_COUNT) {
+            saveData();
+            cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+        }
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        saveData();
+        log.info("All data parsing completed!");
+    }
+
+    private void saveData() {
+        log.info("{} records saved to database!", cachedDataList.size());
+        uploadDAO.save(cachedDataList);
+    }
+}

GOODBOY008/fesod

fesod-examples/fesod-sheet-examples/src/main/java/org/apache/fesod/sheet/examples/read/listeners/ExceptionListener.java
@@ -1,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.sheet.examples.read.listeners;
+
+import com.alibaba.fastjson2.JSON;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.fesod.common.util.ListUtils;
+import org.apache.fesod.sheet.context.AnalysisContext;
+import org.apache.fesod.sheet.examples.read.data.ExceptionDemoData;
+import org.apache.fesod.sheet.exception.ExcelDataConvertException;
+import org.apache.fesod.sheet.metadata.data.ReadCellData;
+import org.apache.fesod.sheet.read.listener.ReadListener;
+
+/**
+ * Listener for handling read and convert exceptions.
+ */
+@Slf4j
+public class ExceptionListener implements ReadListener<ExceptionDemoData> {
+
+    private static final int BATCH_COUNT = 100;
+
+    private List<ExceptionDemoData> cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+
+    @Override
+    public void onException(Exception exception, AnalysisContext context) {
+        log.error("Parsing failed, but continue parsing the next line: {}", exception.getMessage());
+        if (exception instanceof ExcelDataConvertException) {
+            ExcelDataConvertException excelDataConvertException = (ExcelDataConvertException) exception;
+            log.error(
+                    "Parsing exception in row {}, column {}, data: {}",
+                    excelDataConvertException.getRowIndex(),
+                    excelDataConvertException.getColumnIndex(),
+                    excelDataConvertException.getCellData());
+        }
+    }
+
+    @Override
+    public void invokeHead(Map<Integer, ReadCellData<?>> headMap, AnalysisContext context) {
+        log.info("Parsed a header row: {}", JSON.toJSONString(headMap));
+    }
+
+    @Override
+    public void invoke(ExceptionDemoData data, AnalysisContext context) {
+        log.info("Parsed a data row: {}", JSON.toJSONString(data));
+        cachedDataList.add(data);
+        if (cachedDataList.size() >= BATCH_COUNT) {
+            saveData();
+            cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+        }
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        saveData();
+        log.info("All data parsing completed!");
+    }
+
+    private void saveData() {
+        log.info("{} records saved to database!", cachedDataList.size());
+    }
+}

fesod-examples/fesod-sheet-examples/src/main/java/org/apache/fesod/sheet/examples/web/UploadDataListener.java
@@ -1,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.sheet.examples.web;
+
+import com.alibaba.fastjson2.JSON;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.fesod.common.util.ListUtils;
+import org.apache.fesod.sheet.context.AnalysisContext;
+import org.apache.fesod.sheet.read.listener.ReadListener;
+
+/**
+ * Listener for web upload examples.
+ */
+@Slf4j
+public class UploadDataListener implements ReadListener<UploadData> {
+
+    private static final int BATCH_COUNT = 100;
+    private List<UploadData> cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+    private UploadDAO uploadDAO;
+
+    public UploadDataListener(UploadDAO uploadDAO) {
+        this.uploadDAO = uploadDAO;
+    }
+
+    @Override
+    public void invoke(UploadData data, AnalysisContext context) {
+        log.info("Parsed a data row: {}", JSON.toJSONString(data));
+        cachedDataList.add(data);
+        if (cachedDataList.size() >= BATCH_COUNT) {
+            saveData();
+            cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+        }
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        saveData();
+        log.info("All data parsing completed!");
+    }
+
+    private void saveData() {
+        log.info("{} records saved to database!", cachedDataList.size());
+        uploadDAO.save(cachedDataList);
+    }
+}
9989c2fcf560ea3af73254837e78a22857b9ebb6
gongzhongqiang <gongzhongqiang@apache.org>
2025-12-18 19:03:05 +0800

    feat(examples): refactor example module

---
+++ fesod-examples/fesod-sheet-examples/src/main/java/org/apache/fesod/sheet/examples/util/ExampleFileUtil.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.sheet.examples.util;
+
+import java.io.File;
+import java.nio.file.Files;
+import lombok.SneakyThrows;
+
+/**
+ * Utility for example files.
+ */
+public class ExampleFileUtil {
+
+    public static final String EXAMPLE = "example";
+
+    public static String getPath() {
+        java.net.URL resource = ExampleFileUtil.class.getClassLoader().getResource("");
+        if (resource == null) {
+            throw new IllegalStateException("Cannot find classpath root resource");
+        }
+        return resource.getPath();
+    }
+
+    /**
+     * Get the path to a file in the example resource directory.
+     *
+     * @param fileName the file name relative to the example directory (e.g., "demo.xlsx" or "templates/simple.xlsx")
+     * @return the full path to the file
+     */
+    public static String getExamplePath(String fileName) {
+        java.net.URL resource = ExampleFileUtil.class.getClassLoader().getResource(EXAMPLE + "/" + fileName);
+        if (resource != null) {
+            return resource.getPath();
+        }
+        // Fallback to classpath root + example path
+        return getPath() + EXAMPLE + File.separator + fileName;
+    }
+
+    /**
+     * Get the path to write output files in the system temp directory.
+     *
+     * @param fileName the output file name
+     * @return the full path to the output file in temp directory
+     */
+    @SneakyThrows
+    public static String getTempPath(String fileName) {
+        return Files.createTempDirectory("fesod-sheet-examples").toAbsolutePath() + File.separator + fileName;
+    }
+}
